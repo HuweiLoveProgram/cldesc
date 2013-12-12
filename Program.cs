806fb5ab0f81f42de3684b50595fb6e24360500f
Kody Brown <kody@bricksoft.com>
2013-12-12 14:10:20 -0700

    - updated the code a bit.

--- Program.cs
+++ Program.cs
@@ -1,38 +1,44 @@
-﻿//
-// Copyright (C) 2004-2013 Kody Brown (kody@bricksoft.com).
-//
-// MIT License:
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-//
+﻿/*!
+	Copyright (C) 2004-2013 Kody Brown (kody@bricksoft.com).
+	
+	MIT License:
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to
+	deal in the Software without restriction, including without limitation the
+	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	DEALINGS IN THE SOFTWARE.
+*/
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Bricksoft.PowerCode;
 
 namespace cldesc
 {
 	public class Program
 	{
+		private static string appName = "cldesc";
+		private static EnvironmentVariables envars;
+
 		public static int Main( string[] args )
 		{
+			envars = new EnvironmentVariables(appName);
+
 			string file = null;
 			SortedDictionary<string, string> dc;
 			string line;
@@ -47,7 +53,6 @@ namespace cldesc
 			bool sf; // don't delete the backup file
 			bool oc; // output to the console, instead of back to the file
 			Encoding enc; // the encoding used to open the source descript.ion file
-			string appName = "cldesc";
 
 			vf = false;
 			sf = false;
@@ -55,11 +60,11 @@ namespace cldesc
 
 			// Load environment variables first.
 			// Command-line arguments will overwrite them when present.
-			if (EnvironmentVariables.Contains(appName + "_" + "vf")) {
-				vf = EnvironmentVariables.GetBoolean(appName + "_" + "vf");
+			if (envars.contains("vf")) {
+				vf = envars.attr<bool>( "vf");
 			}
-			if (EnvironmentVariables.Contains(appName + "_" + "sf")) {
-				sf = EnvironmentVariables.GetBoolean(appName + "_" + "sf");
+			if (envars.contains( "sf")) {
+				sf = envars.attr<bool>("sf");
 			}
 
 			if (args.Length > 0) {
